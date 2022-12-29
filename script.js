1bdede98d46bf4326f7c83102885dc34e7260cb5
MouadJebbari <jebbari.mouad.solicode@gmail.com>
2022-12-29 07:33:50 -0800

    updat

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-    // Créez une table HTML avec les colonnes correspondant aux différents champs de chaque film
-    const table = document.createElement('table');
-    const headerRow = document.createElement('tr');
-    headerRow.innerHTML = '<th>titre</th><th>réalisateur</th><th>duration</th><th>année de production</th><th>Poster</th><th>Festivals</th><th>Acteurs</th>';
-    table.appendChild(headerRow);
 
-    // Utilisez l'objet XMLHttpRequest pour charger le fichier movies.json
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'movies.json');
-    xhr.onload = () => {
-    if (xhr.status === 200) {
-        // Récupérez les données et parsez-les en utilisant JSON.parse
-        const movies = JSON.parse(xhr.responseText);
+        // Utilisez l'objet XMLHttpRequest pour charger le fichier movies.json
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'movies.json');
+        xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status) {
+                // Récupération des films à partir du fichier JSON
+            var movies = JSON.parse(xhr.responseText);
+            // Génération du HTML pour chaque film
+                var table = '';
+                for (let i = 0; i < movies.length; i++) {
+                    table += `
+                    <tr>
+                        <td>${movies[i].titre}</td>
+                        <td>${movies[i].réalisateur}</td>
+                        <td>${movies[i].durée}</td>
+                        <td>${movies[i].annéedeproduction}</td>
+                        <td><img src="${movies[i].poster}" </td>
+                        <td>${movies[i].festivals}</td>
+                        <td>${movies[i].Acteurs[0].nom},${movies[i].Acteurs[0].prénom},${movies[i].Acteurs[0].nationalité}${'<br>'}
+                        ${movies[i].Acteurs[1].nom},${movies[i].Acteurs[1].prénom},${movies[i].Acteurs[1].nationalité}</td>
+                    <tr>
+                    `;
+                }
+        
+                document.getElementById('tbody').innerHTML= table;
+    
+        // Ajoutez la table à votre page HTML
+            }
+        
+        }
+        xhr.send();
 
-        // Parcourez la liste des films et créez une ligne de tableau HTML pour chaque film
-        movies.forEach(movie => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${movie.titre}</td>
-            <td>${movie.réalisateur}</td>
-            <td>${movie.durée}</td>
-            <td>${movie.annéedeproduction}</td>
-            <td><img src="${movie.poster}"</td>
-            <td>${movie.festivals.join(', ')}</td>
-            <td>${movie.Acteurs.map(Acteurs => `${Acteurs.nom} (${Acteurs.nationalité})`).join(', ')}</td>
-        `;
-        table.appendChild(row);
-        });
-    }
-    };
-    xhr.send();
-
-    // Ajoutez la table à votre page HTML
-    document.body.appendChild(table);
+    
+    
