22a790a0531a90b3e805efa2e21a67b8d7a1ed53
MouadJebbari <jebbari.mouad.solicode@gmail.com>
2023-01-02 07:22:51 -0800

    UPDAT

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-
-        // Utilisez l'objet XMLHttpRequest pour charger le fichier movies.json
+var movies ;
+   
+       // Utilisez l'objet XMLHttpRequest pour charger le fichier movies.json
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'movies.json');
         xhr.onload = () => {
         if (xhr.readyState == 4 && xhr.status) {
+
                 // Récupération des films à partir du fichier JSON
-            var movies = JSON.parse(xhr.responseText);
+            movies = JSON.parse(xhr.responseText);
             // Génération du HTML pour chaque film
                 var table = '';
                 for (let i = 0; i < movies.length; i++) {
@@ -16,20 +18,33 @@
                         <td>${movies[i].durée}</td>
                         <td>${movies[i].annéedeproduction}</td>
                         <td><img src="${movies[i].poster}" </td>
-                        <td>${movies[i].festivals}</td>
-                        <td>${movies[i].Acteurs[0].nom},${movies[i].Acteurs[0].prénom},${movies[i].Acteurs[0].nationalité}${'<br>'}
-                        ${movies[i].Acteurs[1].nom},${movies[i].Acteurs[1].prénom},${movies[i].Acteurs[1].nationalité}</td>
+                        <td><ul>${movies[i].festivals.map(festivals => `<li>${festivals}</li>`).join('')}</ul></td>
+                        <td><ul>${movies[i].Acteurs.map(Acteurs => `<li>${Acteurs.nom}, ${Acteurs.prénom}, ${Acteurs.nationalité}</li>`).join('')}</ul></td>
                     <tr>
                     `;
                 }
-        
                 document.getElementById('tbody').innerHTML= table;
-    
-        // Ajoutez la table à votre page HTML
             }
-        
         }
         xhr.send();
 
-    
-    
+
+     
+        function searchData(value){
+            var table='';
+                for (let i = 0; i < movies.length; i++) {
+                    if(movies[i].titre.toLowerCase().includes(value.toLowerCase())){
+                        table += `
+                        <tr>
+                            <td>${movies[i].titre}</td>
+                            <td>${movies[i].réalisateur}</td>
+                            <td>${movies[i].durée}</td>
+                            <td>${movies[i].annéedeproduction}</td>
+                            <td><img src="${movies[i].poster}" </td>
+                            <td><ul>${movies[i].festivals.map(festivals => `<li>${festivals}</li>`).join('')}</ul></td>
+                            <td><ul>${movies[i].Acteurs.map(Acteurs => `<li>${Acteurs.nom}, ${Acteurs.prénom}, ${Acteurs.nationalité}</li>`).join('')}</ul></td>
+                        <tr>
+                        `;
+                    }
+                    document.getElementById('tbody').innerHTML = table;
+            }}
